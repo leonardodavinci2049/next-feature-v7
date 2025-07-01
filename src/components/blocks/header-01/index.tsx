dc4dc71cb27b7cd5762de98b771edfa10aa7d5f7
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-07-01 19:13:39 -0300

    Adiciona página de produtos com links para detalhes dos produtos

--- src/components/blocks/header-01/index.tsx
+++ src/components/blocks/header-01/index.tsx
@@ -14,8 +14,8 @@ import Link from "next/link";
 import { Suspense } from "react";
 
 const TopMenu = [
+  { name: "Produtos", href: "/products" },
   { name: "Sobre Nós", href: "/about" },
-  { name: "Pricing", href: "#pricing" },
   { name: "FAQ", href: "#faq" },
 ];
 
