71de12a442541e45589cdc30c58d2bd3f147d3a3
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-07-02 18:53:07 -0300

    Adiciona função serverClientFunction e atualiza serverSideFunction para uso de objetos de cliente

---
+++ src/utils/client-utils.ts
@@ -0,0 +1,13 @@
+import "client-only";
+
+export const serverClientFunction = () => {
+
+  console.log(
+    `use multiple libraries,
+       use environment variables,
+       interact with a database,
+       process confidential information`
+  );
+
+  return "server result";
+};
--- src/utils/server-utils.ts
+++ src/utils/server-utils.ts
@@ -2,10 +2,10 @@ import "server-only";
 
 export const serverSideFunction = () => {
   console.log(
-    `use multiple libraries,
-       use environment variables,
-       interact with a database,
-       process confidential information`
+    `use client windows objects,
+        use multiple libraries,
+        use environment variables,
+        interact with a database,`
   );
-  return "server result";
+  return "client result";
 };
